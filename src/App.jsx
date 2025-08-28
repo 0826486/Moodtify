780ae019ee9234f1b28da3c56acf333a7e759599
호정 <w2314@e-mirim.hs.kr>
2025-08-28 23:47:47 +0900

    ✨ 메인 홈 페이지(Home.jsx) 생성 및 메뉴바 import

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Menubar from './components/Menubar'
+import Home from './components/Home'
 
 function App() {
 
   return (
     <div>
       <Menubar />
+      <Home />
     </div>
   )
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Menubar from "../components/Menubar";
+import "../App.css";
+
+function Home() {
+  return (
+    <div>
+      <Menubar />
+    </div>
+  );
+}
+
+export default Home;
