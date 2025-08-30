3704512f332f0db453fe7bdbe272cb1cd1a746c7
호정 <w2314@e-mirim.hs.kr>
2025-08-31 00:08:13 +0900

    ✅ 메모와 레코드 이미지 바 위에 올리기

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,6 +10,14 @@ function Home() {
             <div className="home-image">
                 <img src="/images/bar.png" className="home-img" />
 
+                <div className="memo-bar">
+                    <div className="memo1-container">
+                        <img src="/images/record.png" className="record-img" />
+                        <h1 className="memo1">난 ズl금 음악øłl ㅁı쳐있⊂ト..</h1>
+                    </div>
+                    <h1 className="memo2">왜レF㈛면... 음악○l 좋ブl ㄸĦ문○l⊂ト...</h1>
+                </div>
+
                 <div className="character-bar">
                     <img src="/images/character.png" className="character" />
                 </div>
