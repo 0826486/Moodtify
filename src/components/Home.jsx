a73a5244cbdf8b1a7b5fa0b1a77cb213b9c20506
호정 <w2314@e-mirim.hs.kr>
2025-08-30 23:43:31 +0900

    ✨ 사진 추가 및, 메인 페이지 박스, 캐릭터 추가

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,11 +3,23 @@ import Menubar from "../components/Menubar";
 import "../App.css";
 
 function Home() {
-  return (
-    <div>
-      <Menubar />
-    </div>
-  );
+    return (
+        <div>
+            <Menubar />
+
+            <div className="home-image">
+                <img src="/images/bar.png" className="home-img" />
+
+                <div className="character-bar">
+                    <img src="/images/character.png" className="character" />
+                </div>
+
+                <img src="/images/word.png" className="word" />
+
+            </div>
+
+        </div>
+    );
 }
 
 export default Home;
